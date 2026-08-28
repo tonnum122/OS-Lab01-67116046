9d53bbe7fd1fe8697c6308c841ce5ec23d2ef85c
tonnum122 <159266112+tonnum122@users.noreply.github.com>
2026-08-28 12:03:49 +0000

    Completed Lab 1: System Profiling and Report

--- stress_test.py
+++ stress_test.py
@@ -1,8 +1,5 @@
-# ==========================================
-# OS-Lab 01: CPU Stress Test
-# Student ID: [Enter your ID here]
-# ==========================================
-import time
+import math
 
-# TODO: Write the intensive computation loop here
-# Follow the instructions in the Lab manual.
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+    math.factorial(50000)  # Intensive calculation
